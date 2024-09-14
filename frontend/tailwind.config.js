8747894af71e9c1242105ce9697a26cfab088750
inaamflutterdev <inaamulhaq7847@gmail.com>
2024-09-14 07:48:37 +0500

    Main layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // add your HTML files here
   theme: {
     extend: {},
+    container: {
+      padding: "5rem",
+    }
   },
   plugins: [],
 }
