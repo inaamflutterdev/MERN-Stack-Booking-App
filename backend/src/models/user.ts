2a01e0455a3389a4f943f183d2d3405097bc40ce
inaamflutterdev <inaamulhaq7847@gmail.com>
2024-09-15 01:10:41 +0500

    User register API

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 export type UserType = {
-    _id: String;
-    firstName: String;
-    lastName: String;
-    email: String;
-    password: String;
-}
+  _id: String;
+  firstName: String;
+  lastName: String;
+  email: String;
+  password: String;
+};
 
 const userSchema = new mongoose.Schema({
-    email: {type: String , required: true, unique: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
